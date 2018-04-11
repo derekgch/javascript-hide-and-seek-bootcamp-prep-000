bbfbcc9ecb2fdefeb02516af577919d81dfcf036
derek chen <derek.g.chen@gmail.com>
2018-04-11 17:10:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+
+  const ls = document.querySelector('#grand-node')
+  return ls.querySelector('div').querySelector('div').querySelector('div').querySelector('div')
+
+
+}
+
+function increaseRankBy(n) {
+
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
+
+
+}

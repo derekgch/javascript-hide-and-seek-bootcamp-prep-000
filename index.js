d52ad01dcfde8f0f57f904b167a4c4ca4d89ea09
derek chen <derek.g.chen@gmail.com>
2018-04-11 16:51:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  const lis = document.getElementById('app').querySelectorAll('nested')
+  console.log(lis);
+
+  for (let i = 0; i < lis.length; i++) {
+
+  }
+
+
+
+}
+
+function deepestChild() {
+
+
+
+}
+
+function increaseRankBy(n) {
+
+
+
+
+}
+nestedTarget();
